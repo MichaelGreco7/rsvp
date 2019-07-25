9153403dcb9c023f44efab955814ac1b23bbe12a
Michael Greco <michael.greco07@gmail.com>
2019-07-25 15:04:04 -0400

    4th commit Writing a Handler to Confirm Guests

--- src/Guest.js
+++ src/Guest.js
@@ -6,8 +6,7 @@ const Guest = props =>
         <span>{props.name}</span>
         <label>
             <input
-                type="checkbox"
-                checked={props.isConfirmed} 
+                type="checkbox" checked={props.isConfirmed} 
                 onChange={props.handleConfirmation} /> Confirmed
         </label>
         <button>edit</button>
